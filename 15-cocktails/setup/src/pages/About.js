bf3c00668b9401268d2866d5d0d4b64e74cfda05
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2021-12-29 12:08:26 +0200

    Made 15-cocktails.

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+        obcaecati, veniam harum totam aliquid odit cum doloribus corporis
+        cupiditate iste sapiente laborum necessitatibus quisquam repellat vel
+        error. Repudiandae, nesciunt asperiores.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
